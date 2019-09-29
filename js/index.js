8c947b21f39eee7df9157fb7ccb25a11ebda4d9e
Teison Montero <teisonmontero@MacBook-Air-de-Teison.local>
2019-09-29 00:21:50 -0400

    fix: transition

--- js/index.js
+++ js/index.js
@@ -12,6 +12,12 @@ Scroll('.navbar', 2000, 'bottom', 0, 0);
 Scroll('.header-content-left', 2000, 'top', '300px', 0);
 Scroll('.header-content-right', 2000, 'right', '300px', 0);
 Scroll('.header-bottom', 2000, 'bottom', 0, 1500);
+Scroll('.bloque', 2000, 'bottom', '300px', 0);
+Scroll('.info-left', 2000, 'left', '300px', 0);
+Scroll('.info-rigth', 2000, 'right', '300px', 0);
+Scroll('.info-bottom', 2000, 'bottom', 0, 1500);
+Scroll('.card', 2000, 'left', '300px', 0);
+
 
 //jquerry
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
