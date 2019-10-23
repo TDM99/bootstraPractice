0bcc39491410bf2a37d41e595ed0e225e6dcc901
Teison Montero <teisonmontero@MacBook-Air-de-Teison.local>
2019-10-23 19:13:52 -0400

    Refactor: index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function Scroll(clase, duraction, origin, distance, delay){
+let Scroll = (clase, duraction, origin, distance, delay) =>{
   window.sr = ScrollReveal();
   sr.reveal(clase, {
     duration: duraction,
@@ -18,8 +18,6 @@ Scroll('.info-rigth', 2000, 'right', '300px', 0);
 Scroll('.info-bottom', 2000, 'bottom', 0, 1500);
 Scroll('.card', 2000, 'left', '300px', 0);
 
-
-//jquerry
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
       e.preventDefault();
